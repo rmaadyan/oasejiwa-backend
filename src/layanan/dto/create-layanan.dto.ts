b11db931f6f47be0374c04e1d6c5f35ef15d0068
Adindaokta <Oktaadinda65@gmail.com>
2026-08-11 14:34:08 +0700

    fix: update dto prisma import and soft delete layanan

--- src/layanan/dto/create-layanan.dto.ts
+++ src/layanan/dto/create-layanan.dto.ts
@@ -1,5 +1,5 @@
 import { IsEnum, IsInt, IsOptional, IsString, Min } from 'class-validator';
-import { JenisLayanan, Kategori } from '../../../prisma/generated/enums';
+import { JenisLayanan, Kategori } from '@prisma/client';
 export class CreateLayananDto {
   @IsString()
   nama!: string;
