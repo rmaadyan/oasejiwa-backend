ef7b846bc9e750398ac0efda4c49b5e67570a5b3
Adindaokta <Oktaadinda65@gmail.com>
2026-08-17 07:15:27 +0700

    fix: add EmailModule to PaymentModule imports

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -3,9 +3,10 @@ import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { EmailModule } from '../email/email.module'; // 🟢 1. Import EmailModule
 
 @Module({
-    imports: [PrismaModule, AuthModule],
+    imports: [PrismaModule, AuthModule, EmailModule], // 🟢 2. Masukkan ke imports
     controllers: [PaymentController],
     providers: [PaymentService],
     exports: [PaymentService],
